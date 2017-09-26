a38cccf677ebd64d79e4ee8d30b7739cc6ce1100
RB Metcalf <bmetcalf@gmail.com>
2017-09-26 18:42:07 +0200

    Finished.  It works well and should be integrated into COMOLOGY.

--- correlation_func.cpp
+++ correlation_func.cpp
@@ -31,11 +31,11 @@ static std::mutex barrier;
 
 //**************** test ************************
 
-struct FunctorType1{
-  FunctorType1(COSMOLOGY *cosmo,double radius,double redshift)
+struct FunctorType{
+  FunctorType(COSMOLOGY *cosmo,double radius,double redshift)
   : cosmology(cosmo),z(redshift),r(radius)
   {
-    norm = 0.5/pi/pi;
+    norm = 0.5/pi/pi/(1+z)/(1+z);
   };
   
   COSMOLOGY *cosmology;
@@ -44,15 +44,48 @@ struct FunctorType1{
   
   double norm;
   
-  double operator () (double lnk) {
-
-    double k = exp(lnk);
+  double operator () (double k) {
     
-    return norm*k*k*k*cosmology->powerCDMz(k,z);
+    double rk = r*k;
+    double jo = sin(rk)/rk;
+    if(rk < 1.0e-3) jo = 1.0;
+    
+    return norm*jo*k*k*cosmology->powerCDMz(k,z);
   }
-  
 };
 
+
+struct CorrelationFunction{
+  CorrelationFunction(COSMOLOGY *cosmo,double redshift
+                      ,double k_max = 100,double k_min = 1.0e-3)
+  : cosmology(cosmo),func(cosmo,1.0,redshift),k_max(k_max),k_min(k_min)
+  {
+    if(k_max < k_min) swap(k_min,k_max);
+  };
+  
+  COSMOLOGY *cosmology;
+  FunctorType func;
+  double k_max;
+  double k_min;
+  
+  double operator () (double radius) {
+    double a = pi/radius/2;
+    
+    func.r = radius;
+    
+    double kmin = k_min;
+    double kmax = MIN(a,k_max);
+    double tmp,ans=0;
+    do{
+      tmp = Utilities::nintegrate<FunctorType,double>(func,kmin,kmax,1.0e-4);
+      ans += tmp;
+      kmin = kmax;
+      kmax = MIN(kmax + a,k_max);
+    }while(abs(tmp/ans) > 1.0e-4);
+    
+    return ans;
+  }
+};
   
 
 
@@ -64,8 +97,26 @@ int main(int arg,char **argv){
   Utilities::print_date();
   
   COSMOLOGY cosmo;
+  double r = 1.3,z = 2;
   
+  ofstream outputfile("correlation.csv");
+
+  outputfile << "z,r,C" << std::endl;
+  for(z=0.0;z <= 3;z += 1.0){
+    CorrelationFunction correlation(&cosmo,z);
+
+    for(r=0.001;r<5.0e2;r *=1.05){
+      double corr = correlation(r);
+    
+      outputfile << z << "," << r << "," << corr << std::endl;
+    
+      std::cout << z << " , " << r << " , " << corr << std::endl;
+    }
+  }
   
+  outputfile.close();
+  
+  std::cout << "finished" << std::endl;
 }
 
 
